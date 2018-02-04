893a5152fe06cf6116f0e8e307bed88c13bd48f6
olena-melnik <selena.melnik@gmail.com>
2018-02-04 14:28:39 +0200

    sendEmail

---
+++ language/ru-ru/common/header.php
@@ -0,0 +1,23 @@
+<?php
+// Text
+$_['text_home']          = 'Главная';
+$_['text_wishlist']      = 'Мои закладки (%s)';
+$_['text_shopping_cart'] = 'Корзина покупок';
+$_['text_category']      = 'Категории';
+$_['text_account']       = 'Личный кабинет';
+$_['text_register']      = 'Регистрация';
+$_['text_login']         = 'Авторизация';
+$_['text_order']         = 'История заказов';
+$_['text_transaction']   = 'История платежей';
+$_['text_download']      = 'Файлы для скачивания';
+$_['text_logout']        = 'Выход';
+$_['text_checkout']      = 'Оформление заказа';
+$_['text_search']        = 'Поиск';
+$_['text_all']           = 'Показать все';
+$_['text_page']          = 'страница';
+
+$_['modal_request_call'] = 'Заказать песню';
+$_['modal_btn_send']    = 'Отправить';
+$_['modal_name_placeholder'] = 'Ваше имя*';
+$_['modal_tel_placeholder'] = 'Ваш телефон*';
+$_['modal_text_small'] =' - поля, обязательные для заполнения';
---
+++ language/uk-ua/common/header.php
@@ -0,0 +1,26 @@
+<?php
+//Переклад: Том'як Олег з любов'ю до Української мови та легкості Opencart
+// Text
+$_['text_home']          = 'Головна';
+$_['text_wishlist']      = 'Мої Закладки (%s)';
+$_['text_shopping_cart'] = 'Кошик замовлень';
+$_['text_category']      = 'Категорії';
+$_['text_account']       = 'Особистий кабінет';
+$_['text_register']      = 'Реєстрація';
+$_['text_login']         = 'Авторизація';
+$_['text_order']         = 'Історія замовлень';
+$_['text_transaction']   = 'Історія платежів';
+$_['text_download']      = 'Файли для скачування';
+$_['text_logout']        = 'Вихід';
+$_['text_checkout']      = 'Оформлення замовлення';
+$_['text_search']        = 'Пошук';
+$_['text_all']           = 'Показати всі';
+$_['text_page']          = 'Сторінка';
+$_['text_telephone']           = 'Зв’яжіться з нами     :';
+$_['text_top_head']           = 'Сезон знижок! 70%. Використайте код: “SALE70”. Поспішайте!  > ';
+
+$_['modal_request_call'] = 'Замовити дзвінок';
+$_['modal_btn_send']    = 'Відправити';
+$_['modal_name_placeholder'] = "Ваше им'я*";
+$_['modal_tel_placeholder'] = "Ваш телефон*";
+$_['modal_text_small'] ="*- поля, обов'язкові для заповнення";
